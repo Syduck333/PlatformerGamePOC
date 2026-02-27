6ba99b8ab5f0d2934177eb33fb2020715b471b1f
Syduck333 <smccreary@ftstudent.org>
2026-02-27 08:52:39 -0500

    Added level 2 and new player model
    Level 2 is still in progress but can be accessed

--- scripts/entrancelevel.gd
+++ scripts/entrancelevel.gd
@@ -1,10 +1,11 @@
-extends Area2D
+class_name SceneTrigger extends Area2D
 
 
+@export var connected_scene: String
 
+var scene_folder = "res://scenes/"
 
-func _on_collision_shape_2d_child_entered_tree(node: Node) -> void:
-	pass
-	
-	if CharacterBody2D:
-		pass
+func _on_body_entered(body):
+	var full_path = scene_folder + connected_scene + ".tscn"
+	var scene_tree = get_tree()
+	scene_tree.change_scene_to_file(full_path)
